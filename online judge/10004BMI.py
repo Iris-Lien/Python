ad8af8c4b228fab2e99c0834a338ba8b990d7551
Iris-Lien <jolinqq918@gmail.com>
2020-02-09 17:52:40 +0800

    add loop->done

--- online judge/10004BMI.py
+++ online judge/10004BMI.py
@@ -1,11 +1,14 @@
-#people = input()
-weight, height = input().split()
-bmi = float(weight)/(float(height)*float(height))
-if bmi<18.5:
-    print('under')
-elif 18.5<=bmi<25.0:
-    print('normal')
-elif 25.0<=bmi<30.0:
-    print('over')
-else:
-    print('obese')
+people = int(input())
+n = 1
+while n <= people:
+    weight, height = input().split()
+    bmi = float(weight)/(float(height)*float(height))
+    if bmi<18.5:
+        print('under')
+    elif 18.5<=bmi<25.0:
+        print('normal')
+    elif 25.0<=bmi<30.0:
+        print('over')
+    else:
+        print('obese')
+    n += 1
