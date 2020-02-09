a523051cd9d5187540163ba2f4384d5baccbb045
Iris-Lien <jolinqq918@gmail.com>
2020-02-09 18:36:01 +0800

    correct

--- online judge/10002Password.py
+++ online judge/10002Password.py
@@ -3,21 +3,6 @@ sum_a = (int(a)//100)+(int(a)%100)//10+(int(a)%10)
 sum_b = (int(b)//100)+(int(b)%100)//10+(int(b)%10)
 sum_c = (int(c)//100)+(int(c)%100)//10+(int(c)%10)
 sum_all = 10*(sum_a+sum_b)+sum_c
-
-if 100<=sum_all<=999:#三位數
-    sum_allrev=(int(sum_all)%10)*100+((int(sum_all)%100)//10*10)+int(sum_all)//100
-    print(sum_allrev)
-elif sum_all>999: #超過三位數
-    sum_all = "%03d" % sum_all
-    sum_allrev=(int(sum_all)%10)*100+((int(sum_all)%100)//10*10)+int(sum_all)//100
-    sum_allrev = "%03d" % sum_allrev
-    print(sum_allrev)
-else:#不足三位數
-    if 10<=sum_all<=99:
-        sum_all = sum_all*10
-    if sum_all<10:
-        sum_all = sum_all*100
-    sum_all = "%03d" % sum_all
-    sum_allrev=(int(sum_all)%10)*100+((int(sum_all)%100)//10*10)+int(sum_all)//100
-    sum_allrev = "%03d" % sum_allrev
-    print(sum_allrev)
+sum_allrev = str(sum_all)[::-1]
+sum_allrev = "%03d" % int(sum_allrev)
+print(sum_allrev)
